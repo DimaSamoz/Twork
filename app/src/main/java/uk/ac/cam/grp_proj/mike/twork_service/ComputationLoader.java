ebcecafe101259abbc71460781f43f5ec0174e1d
laMudri <lamudri@gmail.com>
2016-02-16 12:32:14 +0000

    added some error handling to JobFetchExample and added some classes I was working on

---
+++ app/src/main/java/uk/ac/cam/grp_proj/mike/twork_service/ComputationLoader.java
@@ -0,0 +1,11 @@
+package uk.ac.cam.grp_proj.mike.twork_service;
+
+/**
+ * Created by James on 10/02/16.
+ */
+public class ComputationLoader extends ClassLoader {
+    @Override
+    public Class<?> loadClass(String name) throws ClassNotFoundException {
+        ;
+    }
+}
