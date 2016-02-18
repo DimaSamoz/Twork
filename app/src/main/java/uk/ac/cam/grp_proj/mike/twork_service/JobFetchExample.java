33005b0b69b9da8846566e11adb654787fa1d488
laMudri <lamudri@gmail.com>
2016-02-18 11:36:08 +0000

    send MAC address with job request

--- app/src/main/java/uk/ac/cam/grp_proj/mike/twork_service/JobFetchExample.java
+++ app/src/main/java/uk/ac/cam/grp_proj/mike/twork_service/JobFetchExample.java
@@ -28,7 +28,7 @@ public class JobFetchExample {
     private static long timeout = 1000;
 
     public static void doJob(Context context) throws InterruptedException {
-        String hostURL = "http://52.11.247.46:9000/";
+        String hostURL = "http://ec2-52-36-182-104.us-west-2.compute.amazonaws.com:9000";
 
         //Send GET /available
         //At some point this will contain JSON about the phone, but it can be empty for now.
