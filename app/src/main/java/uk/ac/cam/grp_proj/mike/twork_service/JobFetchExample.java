e1096ddb855b55f754e59c7068e97e0e637f9237
laMudri <lamudri@gmail.com>
2016-02-18 12:00:21 +0000

    fixed server URL again

--- app/src/main/java/uk/ac/cam/grp_proj/mike/twork_service/JobFetchExample.java
+++ app/src/main/java/uk/ac/cam/grp_proj/mike/twork_service/JobFetchExample.java
@@ -11,20 +11,55 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.StringWriter;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarOutputStream;
+
+import dalvik.system.DexClassLoader;
 
 
 public class JobFetchExample {
     private static final String TAG = "JobFetchExample";
     private static long timeout = 1000;
+
+    private static InputStream urlInputStream(URL url, String cookie) throws InterruptedException {
+        while (true) {
+            try {
+                HttpURLConnection compCon = (HttpURLConnection) url.openConnection();
+                compCon.setRequestProperty("Cookie", cookie);
+                compCon.connect();
+                int compResp = compCon.getResponseCode();
+                Log.i(TAG, "Response: " + Integer.toString(compResp));
+                switch (compResp) {
+                    case 200:
+                        return compCon.getInputStream();
+                    case 204:
+                        Thread.sleep(timeout);
+                        break;
+                    default:
+                        Log.w(TAG, "weird response code " + Integer.toString(compResp));
+                        break;
+                }
+            }
+            catch (IOException e) {
+                Log.e(TAG, "", e);
+            }
+        }
+    }
 
     public static void doJob(Context context) throws InterruptedException {
         String hostURL = "http://52.36.156.147:9000/";
@@ -83,44 +118,48 @@ public class JobFetchExample {
 			    */
                 //###
 
-                //Send GET /job with cookie
-                URL jobURL = new URL(hostURL + "job");
-                HttpURLConnection jobCon = (HttpURLConnection) jobURL.openConnection();
-                jobCon.setRequestProperty("Cookie", cookie);
-                jobCon.connect();
-                int responseCode = jobCon.getResponseCode();
-                Log.i(TAG, "Job response: " + jobCon.getResponseCode());
-                switch (responseCode) {
-                    case 200:
-                        // Has job
-                        break;
-                    case 204:
-                        // No jobs
-                        Thread.sleep(timeout);
-                        continue;
-                    default:
-                        Log.w(TAG, "weird response code " + Integer.toString(responseCode));
-                        continue;
-                }
-
                 //Parse the JSON describing the job
-                InputStream in = jobCon.getInputStream();
+                InputStream in = urlInputStream(new URL(hostURL + "job"), cookie);
                 StringWriter writer = new StringWriter();
                 IOUtils.copy(in, writer, StandardCharsets.UTF_8);
                 String str = writer.toString();
 
+                JSONObject j = new JSONObject(str);
+                String functionName = j.getString("function-class");
+                long jobID = j.getLong("job-id");
+
+                // NB: This whole section is skeptical.
+                // Delete to END and replace invoke line if we're not doing .class downloading.
+                // Remove the rest of the stuff from the try block, too.
+                long compID = j.getLong("comp-id");
+
                 try {
-                    JSONObject j = new JSONObject(str);
-                    String functionName = j.getString("function-class");
-                    long jobID = j.getLong("job-id");
+                    File codeDir = context.getCacheDir();
+                    String[] codeFiles = codeDir.list();
+                    String baseFileName = "comp" + Long.toHexString(compID);
+                    if (!Arrays.asList(codeFiles).contains(baseFileName + ".jar")) {
+                        try (InputStream compIn =
+                                     urlInputStream(new URL(hostURL + "test/code"), cookie);
+                             FileOutputStream compOut =
+                                     new FileOutputStream(codeDir + baseFileName + ".class")) {
+                            IOUtils.copy(compIn, compOut);
+                        } catch (IOException e) {
+                            Log.e(TAG, "", e);
+                        }
+                        JarEntry entry = new JarEntry(codeDir + baseFileName + ".class");
+                        JarOutputStream jarOut = new JarOutputStream(
+                                new FileOutputStream(codeDir + baseFileName + ".jar"));
+                        jarOut.putNextEntry(entry);
+                    }
 
-                    //Load and instantiate the class
-                    //TerribleURLClassLoader loader = new TerribleURLClassLoader(new URL(hostURL + "test/code/"));
-                    //ClassLoader loader = ClassLoader.getSystemClassLoader();
-                    //Class<?> codeClass = loader.loadClass("uk.ac.cam.grp_proj.mike.twork_service." + functionName);
-                    //		Object o = codeClass.newInstance();
-                    //Method codeToRun = codeClass.getDeclaredMethod("run", new Class<?>[] {InputStream.class, OutputStream.class});
-
+                    DexClassLoader loader = new DexClassLoader(codeDir.getPath() + "comp.jar",
+                            context.getCodeCacheDir().getPath(), "",
+                            ClassLoader.getSystemClassLoader());
+                    Class<?> codeClass = loader.loadClass(baseFileName);
+                    Object o = codeClass.newInstance();
+                    Method codeMethod = codeClass.getDeclaredMethod("run",
+                            new Class<?>[] {InputStream.class, OutputStream.class});
+                    // END
 
                     //Get the data for the job
                     URL dataURL = new URL(hostURL + "data/" + Long.toString(jobID));
@@ -134,13 +173,12 @@ public class JobFetchExample {
                     ByteArrayOutputStream jobOutput = new ByteArrayOutputStream();
 
                     //Run the job
-                    //codeToRun.invoke(o, jobInput, jobOutput);
-
                     SecurityManager oldSM = System.getSecurityManager();
                     HashSet<String> filePaths = new HashSet<>();
                     // TODO: give some scratch space
                     System.setSecurityManager(new WorkSecurityManager(filePaths));
-                    new PrimeComputationCode().run(jobInput, jobOutput);
+                    codeMethod.invoke(o, jobInput, jobOutput);
+                    //new PrimeComputationCode().run(jobInput, jobOutput);
                     System.setSecurityManager(oldSM);
 
                     //Get the output from the job
@@ -159,14 +197,18 @@ public class JobFetchExample {
                     osw.write(outStr.getBytes(StandardCharsets.UTF_8));
                     osw.close();
                     Log.i(TAG, "Result response: " + resultCon.getResponseCode());
-                } catch (JSONException e) {
-                    Log.e(TAG, "bad response", e);
                 }
+                catch (ClassNotFoundException | NoSuchMethodException | InstantiationException
+                        | IllegalAccessException | InvocationTargetException e) {
+                    Log.e(TAG, "", e);
+                }
+            } catch (JSONException e) {
+                Log.e(TAG, "bad response", e);
             }
             catch (IOException e) {
                 Log.e(TAG, "", e);
             }
-		}
-	}
+        }
+    }
 }
 
